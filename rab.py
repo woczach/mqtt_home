48a28d65cb6f357b46fe8cb84d5663e27d049fa3
Your Name <parasol255>
2024-03-08 17:44:38 +0100

    s

---
+++ rab.py
@@ -0,0 +1,25 @@
+import pika
+
+#connection = pika.BlockingConnection(pika.ConnectionParameters('localhost'))
+#channel = connection.channel()
+
+
+def main():
+    connection = pika.BlockingConnection(pika.ConnectionParameters(host='localhost'))
+    channel = connection.channel()
+
+#    channel.queue_declare(queue='all')
+
+    def callback(ch, method, properties, body):
+        print(f"-> Received {body}")
+        print(f"-> prop {properties}")
+        print(f"-> ch {ch}")
+        print(f"->  met {method}")
+        print()
+
+    channel.basic_consume(queue='all', on_message_callback=callback, auto_ack=True)
+
+    print(' [*] Waiting for messages. To exit press CTRL+C')
+    channel.start_consuming()
+
+main()
