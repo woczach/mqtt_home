575706c801d53a4547871c90c573811e717eeee8
Wojciech Oczachowski <parasol255@o2.pl>
2024-10-25 11:30:22 +0200

    master-13648

--- ebusd-z2m-home/z2m_monitor.py
+++ ebusd-z2m-home/z2m_monitor.py
@@ -1,10 +1,10 @@
 
 import random
-from libs import connect_mqtt, publish, measurments_preparation_sent, return_current_settings, return_current_temps
+from libs import connect_mqtt, publish, measurments_preparation_sent, return_current_settings, return_current_temps, push_to_db
 import pika
 from time import sleep
 import threading
-
+import json
 
 broker = '192.168.0.230'
 port = 11883
@@ -13,7 +13,10 @@ client_id = f'publish-{random.randint(0, 100)}'
 timeout = 5 * 60
 
 
-
+OFF_VALVE_TEMP = 16.0
+ON_VALVE_TEMP = 24.0
+WODA = 42
+WODAOFF= 0
 
 topics_z2m = {
     'Salon': "0xa4c1384e6bcc60c6", 
@@ -23,24 +26,103 @@ topics_z2m = {
     'Sypialnia': "0x94deb8fffe2e751a"
 }
 
-podiff = {0.25: 45, 0.5:50, 2:60}
-jodiff = {0.5: 40, 1: 45, 2: 50}
+podiff = {-0.25: 45, -0.5:50, -2:60}
+jodiff = {-0.5: 40, -1: 45, -2: 50}
 
 
         # 'Jozef': {'measurment': 'Tasmota_switch', 'value_name': 'temperatura'},
         # 'Kuchnia': {'measurment':  'esp/sypialnia', 'value_name': 'value'},
         # 'Salon': {'measurment':  'rpi_temperatura', 'value_name': 'celsius'},
         # 'Sypialnia': {'measurment':  'esp/kuchnia', 'value_name': 'value'}
-  
+
+
+def calculate_settings(temp_diff):
+    PIEC_OFF = 1
+    settemp = 40
+
+    print(temp_diff)
+    close_valve = [k for k, v in temp_diff.items() if v >= 0]
+    open_valve = [k  for k, v in topics_z2m.items() if k not in close_valve]
+    smallest_key = min(temp_diff, key=temp_diff.get)
+    smallest_value = temp_diff[smallest_key]
+
+    if smallest_value < 0:
+        PIEC_OFF = 0
+        if smallest_key == 'Jozef':
+            for k,v in jodiff.items():
+                if (smallest_value) < k:
+                    settemp = v
+        else:            
+            for k,v in podiff.items():
+                if (smallest_value) < k:
+                    settemp = v
+    else:
+        PIEC_OFF = 1
+    
+    print(f'close valve = {close_valve}, open valve = {open_valve}, smallest diff {smallest_key}, smallest value {smallest_value}')
+    print(f'piec off = {PIEC_OFF}, settemp = {settemp}')
+    fields = {}
+    client = connect_mqtt(client_id, broker, port)
+    client.loop_start()        
+    for close in close_valve:
+        fields[close] = OFF_VALVE_TEMP
+        full_topic = f'zigbee2mqtt/{topics_z2m[close]}/set'
+        message = json.dumps({"current_heating_setpoint": str(OFF_VALVE_TEMP)})
+        publish(client, full_topic, message)
+    for open in open_valve:
+        fields[open]= ON_VALVE_TEMP        
+        full_topic = f'zigbee2mqtt/{topics_z2m[open]}/set'
+        message = json.dumps({"current_heating_setpoint":  str(ON_VALVE_TEMP)})
+        publish(client, full_topic, str(message))
+    if PIEC_OFF == 0:
+        fields['Heating_temp'] = settemp
+        message_to_piec = f'0;{settemp};{WODA};-;-;{PIEC_OFF};0;{WODAOFF};-;0;0;0'   
+        topic_piec = 'ebusd/BAI/SetModeOverride/set'
+        publish(client, topic_piec, message_to_piec)
+    else:
+        fields['Heating_temp'] = 0
+        message_to_piec = f'0;{settemp};{WODA};-;-;{PIEC_OFF};0;{WODAOFF};-;0;0;0'   
+        topic_piec = 'ebusd/BAI/SetModeOverride/set'
+        publish(client, topic_piec, message_to_piec)
+    client.loop_stop()     
+
+    connection = {'URL': '192.168.0.230', 'PORT': 8086, "DBUSER": "username", "DBPASS": 'password'}   
+    data_point = [
+        {
+        "measurement": "settings",
+        "fields": fields
+        }
+    ]
+    print(data_point)
+    push_to_db('heat', data_point, connection) 
+
+      
+
+
+    # for pokoj, valve in topics_z2m.items():
+    #     if pokoj not in temp_diff:
+    #         open_valve.append(valve)
+    #     else:    
+    #         if temp_diff[pokoj] >= 0:
+    #             close_valve.append(valve)
+    #             #close_valve
+    #         else:
+    #             open_valve.append(valve)
+    #             if temp_diff[pokoj] < smallest_diff:
+    #                 smallest_diff = temp_diff[pokoj]
+    #             PIEC_OFF = 0
+    # if time_of_day == 'Night':
+    #     temp_curve = jodiff
+    # else:
+    #     temp_curve = podiff
+
+        
 
 def regulate_temp():
     while True:
 
         settings, time_of_day = return_current_settings()
-        if time_of_day == 'Night':
-            temp_curve = jodiff
-        else:
-            temp_curve = podiff
+
             
         print(settings)
         current_temps = return_current_temps()
@@ -51,21 +133,8 @@ def regulate_temp():
             if settings[pokoj] != 0:
                 temp_diff[pokoj] = current_temps[pokoj][0]['value'] - set_temp
                 print(f"pokoj {pokoj} set temp {set_temp} current temp  {current_temps[pokoj][0]['value']} temp {temp_diff[pokoj]}")
-        PIEC_OFF = 1
-        smallest_diff = 0
-        close_valve = []
-        open_valve = []
-        for pokoj, diff in temp_diff.items():
-            
-            if diff >= 0:
-                close_valve.append(topics_z2m[pokoj])
-                #close_valve
-            else:
-                open_valve.append(topics_z2m[pokoj])
-                if diff < smallest_diff:
-                    smallest_diff = diff
-                PIEC_OFF = 0
-        
+
+        calculate_settings(temp_diff)
 
         sleep(120)
 
