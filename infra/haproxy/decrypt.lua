9073a45efd5f5e0218be8c861c15511039352c51
Wojciech Oczachowski <parasol255@o2.pl>
2024-12-23 17:41:48 +0100

    test

---
+++ infra/haproxy/decrypt.lua
@@ -0,0 +1,25 @@
+
+  
+local  function decode(txn)
+    str = txn.get_var(txn,"req.to_decode")  
+    core.Alert(string.format("to be decoded  is %s", str))
+    Key53 = 8186484168865098
+    Key14 = 4887      
+    local K, F = Key53, 16384 + Key14
+    return (str:gsub('%x%x',
+      function(c)
+        local L = K % 274877906944  -- 2^38
+        local H = (K - L) / 274877906944
+        local M = H % 128
+        c = tonumber(c, 16)
+        local m = (c + (H - M) / 128) * (2*M + 1) % 256
+        K = L * F + H + c + m
+        core.Alert(string.format("decoded is %s", m))
+        txn.set_var(txn,'req.decoded',m)
+        return string.char(m)
+      end
+    ))
+  end
+end
+  
+core.register_action("decode",{'http-req'}, decode, 0)
