8fec4508e872d8c770984183eb7abffe2f798582
Wojciech Oczachowski <parasol255@o2.pl>
2024-12-28 08:56:15 +0100

    test

--- infra/haproxy/decode.lua
+++ infra/haproxy/decode.lua
@@ -4,8 +4,38 @@ do
   local Key14 = 4887
 
   local inv256
+  local b='ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/' -- You will need this for encoding/decoding
 
-  function encode(str)
+  function b64enc(data)
+    return ((data:gsub('.', function(x) 
+        local r,b='',x:byte()
+        for i=8,1,-1 do r=r..(b%2^i-b%2^(i-1)>0 and '1' or '0') end
+        return r;
+    end)..'0000'):gsub('%d%d%d?%d?%d?%d?', function(x)
+        if (#x < 6) then return '' end
+        local c=0
+        for i=1,6 do c=c+(x:sub(i,i)=='1' and 2^(6-i) or 0) end
+        return b:sub(c+1,c+1)
+    end)..({ '', '==', '=' })[#data%3+1])
+end
+
+-- decoding
+function b64dec(data)
+    data = string.gsub(data, '[^'..b..'=]', '')
+    return (data:gsub('.', function(x)
+        if (x == '=') then return '' end
+        local r,f='',(b:find(x)-1)
+        for i=6,1,-1 do r=r..(f%2^i-f%2^(i-1)>0 and '1' or '0') end
+        return r;
+    end):gsub('%d%d%d?%d?%d?%d?%d?%d?', function(x)
+        if (#x ~= 8) then return '' end
+        local c=0
+        for i=1,8 do c=c+(x:sub(i,i)=='1' and 2^(8-i) or 0) end
+            return string.char(c)
+    end))
+end
+
+  function encrypt(str)
     if not inv256 then
       inv256 = {}
       for M = 0, 127 do
@@ -59,12 +89,17 @@ function split_string(input_str, delimiter)
 end
 
 function return_permissions(str)
-  return split_string(decode(str),'-')[2]
+  return split_string(b64dec(decode(str)),'-')[2]
 end
 
 function return_timestamp(str)
-  return split_string(decode(str),'-')[1]
+  return split_string(b64dec(decode(str)),'-')[1]
 end
+
+function encode(str)
+  return encrypt(b64enc(str))
+end
+
 
 -- local s = 'Hello world'
 -- print(       encode(s) ) --> 80897dfa1dd85ec196bc84
